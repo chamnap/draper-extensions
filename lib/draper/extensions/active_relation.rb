855d90ae1704540f4b9640d039ca0283c64fd43c
Chamnap Chhorn <chamnapchhorn@gmail.com>
2014-05-14 16:31:35 +0700

    fix when activerecord doesn't load

--- lib/draper/extensions/active_relation.rb
+++ lib/draper/extensions/active_relation.rb
@@ -3,13 +3,12 @@
 ## @products = listing.products.page(1).decorate
 ## @products => Draper::CollectionDecorator
 
-if defined?(ActiveRecord) &&
-  ActiveRecord::VERSION::STRING.start_with?("4.") ||
-  ActiveRecord::VERSION::STRING.start_with?("3.2")
-
-  class ActiveRecord::Relation
-    def decorate
-      klass.decorator_class.decorate_collection(self)
+if defined?(ActiveRecord)
+  if ActiveRecord::VERSION::STRING.start_with?("4.") || ActiveRecord::VERSION::STRING.start_with?("3.2")
+    class ActiveRecord::Relation
+      def decorate
+        klass.decorator_class.decorate_collection(self)
+      end
     end
   end
 end
